f4d4afe37eac550984049eb125fc38d41868d082
Minsung Kim <spongbob9876@naver.com>
2023-10-12 21:53:55 +0000

    Enhanced script with `getopt`

--- run.sh
+++ run.sh
@@ -1,30 +1,64 @@
 #!/bin/bash
 
 # https://stackoverflow.com/questions/59895/how-do-i-get-the-directory-where-a-bash-script-is-located-from-within-the-script
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+SCRIPT_DIR=$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)
 cd $SCRIPT_DIR
 
-if [[ $# -lt 2 ]]; then
-    echo "Usage: $0 server_name version"
+OPTS=$(getopt --longoptions Xmx,Xms --name "$0" -- "$@" &> /dev/null)
+
+function die() {
+    echo "$1"
     exit 1
-elif [[ $1 == "server_archives" ]]; then
-    echo "\"server_archives\" as server_name is not allowed."
-    exit 1
-elif [[ ! -d "$SCRIPT_DIR/$1" ]]; then
-    echo "Server directory $SCRIPT_DIR/$1 does not exist."
-    exit 1
+}
+
+if [[ $# -lt 1 ]]; then
+    die "Usage: $0 SERVER_NAME [--version VERSION] [--Xmx MAX_MEMORY] [--Xms MIN_MEMORY]"
+elif ! command -v java &> /dev/null; then
+    die "Java not found"
 fi
 
-JARFILE=$(ls "$SCRIPT_DIR/$1" | grep ".*$2.*.jar" | head -1)
+SERVER_NAME=$1
+shift 2
+
+VERSION="1.20"
+Xmx="1536M"
+Xms="1024M"
+
+while true; do
+    case "$0" in
+        --Xmx)
+            Xmx=$1
+            shift 2
+            ;;
+        --Xms)
+            Xms=$1
+            shift 2
+            ;;
+        --version)
+            VERSION=$1
+            shift 2
+            ;;
+        *)
+            break
+    esac
+done
+
+if [[ $SERVER_NAME == "server_archives" ]]; then
+    die "\"server_archives\" as server_name is not allowed."
+elif [[ ! -d "$SCRIPT_DIR/$SERVER_NAME" ]]; then
+    die "Server directory $SCRIPT_DIR/$SERVER_NAME does not exist."
+fi
+
+JARFILE=$(ls "$SCRIPT_DIR/$SERVER_NAME" | grep ".*$VERSION.*.jar" | head -1)
 
 if [[ ! $JARFILE ]]; then
-    echo "Couldn't find \"$2\" version of java server file in the server directory."
-    exit 1
+    die "Couldn't find \"$VERSION\" version of java server file in the server directory."
 fi
 
-if [[ ! -f "$SCRIPT_DIR/$1/eula.txt" ]]; then
+if [[ ! -f "$SCRIPT_DIR/$SERVER_NAME/eula.txt" ]]; then
     echo "Copying prepared EULA file.."
-    cp "$SCRIPT_DIR/server_archives/eula_copy.txt" "$SCRIPT_DIR/$1/eula.txt"
+    cp "$SCRIPT_DIR/server_archives/eula_copy.txt" "$SCRIPT_DIR/$SERVER_NAME/eula.txt"
 fi
 
-cd $1 && java -Xmx1536M -Xms512M -jar $JARFILE nogui
+cd $SERVER_NAME
+java -Xmx$Xmx -Xms$Xms -jar $JARFILE nogui
